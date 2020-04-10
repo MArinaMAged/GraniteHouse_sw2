83225845d447e42eaa50c71a754a3a2291e89dbc
abanoub-girges <abanoub.girges2@gmail.com>
2020-04-11 02:13:26 +0300

    Add AppointmentviewModel& Appointmentscontroller
    Co-Authored-By: Abanoub Girges <abanoub.girges2@gmail.com>

---
+++ GraniteHouse_sw2/GraniteHouse_sw2/Areas/Admin/Controllers/AppointmentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse_sw2.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse_sw2.Areas.Admin.Controllers
+{
+    [Authorize(Roles = SD.SuperAdminEndUser + "," + SD.SuperAdminEndUser)]
+    [Area("Admin")]
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
